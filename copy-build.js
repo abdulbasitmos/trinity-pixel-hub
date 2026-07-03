10e7b1e11c1d0128284e284cb135cec3f013ffb6
abdulbasitmos <abdulbasitmoshood6@gmail.com>
2026-07-03 17:10:05 +0100

    new

---
+++ copy-build.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const path = require('path');
+
+function copyFolderSync(from, to) {
+  if (!fs.existsSync(to)) {
+    fs.mkdirSync(to, { recursive: true });
+  }
+  fs.readdirSync(from).forEach(element => {
+    const stat = fs.lstatSync(path.join(from, element));
+    if (stat.isFile()) {
+      fs.copyFileSync(path.join(from, element), path.join(to, element));
+    } else if (stat.isDirectory()) {
+      copyFolderSync(path.join(from, element), path.join(to, element));
+    }
+  });
+}
+
+const source = path.join(__dirname, 'trinity-pixel-hub', 'dist');
+const destination = path.join(__dirname, 'dist');
+
+try {
+  console.log(`Copying build outputs from ${source} to ${destination}...`);
+  if (fs.existsSync(destination)) {
+    fs.rmSync(destination, { recursive: true, force: true });
+  }
+  copyFolderSync(source, destination);
+  console.log('Build outputs successfully copied to root directory!');
+} catch (err) {
+  console.error('Failed to copy build outputs:', err);
+  process.exit(1);
+}
